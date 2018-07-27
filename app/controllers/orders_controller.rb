e80de1de8bc72f6a0052cd36b066b6ab07230c59
TheanRogerio <thean.rogerio@gmail.com>
2018-07-27 17:57:14 -0300

    Implement update action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+	before_action :set_order, only: [:edit, :update]
+
 	def index
 		@orders = Order.all
 	end
@@ -13,8 +15,21 @@ class OrdersController < ApplicationController
 		redirect_to orders_path, notice: 'Pedido criado com sucesso.' if @order.save
 	end
 
+	def edit
+	end
+	
+	def update
+		if @order.update(order_params)
+			redirect_to orders_path, notice: 'Pedido atualizado com sucesso.'
+		end
+	end
+
 	private
+		def set_order
+			@order = Order.find(params[:id])
+		end
+
 		def order_params
-			params.require(:order).permit(:total, :status)
+			params.require(:order).permit(:total, :status, )
 		end
 end

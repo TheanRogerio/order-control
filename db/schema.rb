2658ee365994320c69506adafd3e9ae2e1ec482d
TheanRogerio <thean.rogerio@gmail.com>
2018-07-28 17:43:11 -0300

    Implement order statuses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_180632) do
+ActiveRecord::Schema.define(version: 2018_07_28_202215) do
 
   create_table "order_items", force: :cascade do |t|
     t.string "description"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2018_07_28_180632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+statuses = OrderStatus.create([
+	{ name: 'Aprovado' },
+	{ name: 'Faturado' },
+	{ name: 'Entregue' },
+	{ name: 'Cancelado' }
+])
